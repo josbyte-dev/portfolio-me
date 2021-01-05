8d7f22586f042ffa8576ad3208c64cb0dfbe0872
dehorus <ruizm.aj@gmail.com>
2021-01-05 16:00:15 -0600

    maquetation closed

--- app-js.js
+++ app-js.js
@@ -126,17 +126,22 @@ let x = window.matchMedia("(min-width: 585px)");
 /* SLIDES */
 
 
-var slideIndex = 0;
+let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let slides2 = document.getElementsByClassName("mySlides-2");
+  for (i = 0; i < slides.length && slides2.length; i++) {
     slides[i].style.display = "none";
+    slides2[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  if (slideIndex > slides2.length) { slideIndex = 1 }
+  slides2[slideIndex - 1].style.display = "block";
+
   setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
